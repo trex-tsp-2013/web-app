e684d4619b07584355fcb78fb3f131c6f3e143ec
tbear04 <gigar_destroyer@msn.com>
2013-11-12 13:56:37 +0700

    Update form13.php
    minor fixed

--- form13.php
+++ form13.php
@@ -25,7 +25,7 @@
         }
       }
     </style>
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
